cea6365fb85dfdfcb5fda35da781503a9644c991
Rob <robianmcd@gmail.com>
2016-04-17 11:09:47 -0400

    added node server for statically serving files

---
+++ server.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 8080;
+
+app.use(express.static(__dirname));
+
+app.all('*', function (req, res) {
+    res.sendFile('index.html', {root: __dirname});
+});
+
+app.listen(port);
+console.log('Listening on port ' + port);
